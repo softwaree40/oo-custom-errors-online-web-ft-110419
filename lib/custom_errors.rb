6d942b4194ecf28cc2d4b25def89bd249720ac6c
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-30 23:38:50 +0000

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,8 +8,15 @@ class Person
   def get_married(person)
     self.partner = person
     person.partner = self
-  end
+    if person.class != Person
+    raise PartnerError
+    else
+    person.partner = self
+    end
 
+end
+  class PartnerError < StandardError
+  end
 end
 
 beyonce = Person.new("Beyonce")
